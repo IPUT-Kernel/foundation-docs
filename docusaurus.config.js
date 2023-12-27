1ef9e22c74bac66fb4f205590db695b47226f269
koki-takada-1 <koukigaotian39@gmail.com>
2023-12-28 06:49:40 +0900

    ブログの編集

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,10 +47,8 @@ const config = {
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          blogSidebarTitle: 'ブログ一覧',
+          truncateMarker: /<!--\s*(more)\s*-->/,
         },
         theme: {
           customCss: './src/css/custom.css',
